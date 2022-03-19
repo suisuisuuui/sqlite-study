7ccba2e814d5716e0b86ac6dc6dbb8bff97ea902
suisuisuuui <Suisuisuuui@example.com>
2022-03-19 11:15:54 +0900

    update package.json
    Define type

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import Database from 'better-sqlite3';
+import BetterSqlite3 from 'better-sqlite3';
 
 ///////////////////////////////
 // Open Database
 ///////////////////////////////
-const db = new Database('test.db', { verbose: console.log });
+const db: BetterSqlite3.Database = new BetterSqlite3('test.db', { verbose: console.log });
 
 ///////////////////////////////
 // Create Table
@@ -13,10 +13,11 @@ db.exec("CREATE TABLE IF NOT EXISTS users('name' varchar PRIMARY KEY, 'age' INTE
 ///////////////////////////////
 // Insert
 ///////////////////////////////
-const insert = db.prepare('INSERT INTO users (name, age) VALUES (@name, @age)');
+const insert: BetterSqlite3.Statement
+    = db.prepare('INSERT INTO users (name, age) VALUES (@name, @age)');
 
 // 1. Use 'for' statement
-const insertMany = db.transaction((users) => {
+const insertMany: BetterSqlite3.Transaction = db.transaction((users) => {
     for (const user of users) insert.run(user);
 });
 
@@ -27,12 +28,12 @@ insertMany([
 ]);
 
 // 2. Not use transaction
-insert.run({name: 'AAA',age: 5});
+insert.run({ name: 'AAA', age: 5 });
 
 // 3. Standard transaction
-const trantest = db.transaction(() => {
+const trantest: BetterSqlite3.Transaction = db.transaction(() => {
     // BEGIN
-    insert.run({name: 'BBB',age: 5});
+    insert.run({ name: 'BBB', age: 5 });
     // COMMIT
     // IF error, ROLLBACK
 });
@@ -43,33 +44,33 @@ trantest();
 // Get
 ///////////////////////////////
 // 1. Get All
-const stmt = db.prepare('SELECT * FROM users;');
-console.log(stmt.all());
+const get1: BetterSqlite3.Statement = db.prepare('SELECT * FROM users;');
+console.log(get1.all());
 
 // 2. Get One
-const stmt2 = db.prepare("SELECT * FROM users WHERE name = 'BBB';");
-console.log(stmt2.all());
+const get2: BetterSqlite3.Statement = db.prepare("SELECT * FROM users WHERE name = 'BBB';");
+console.log(get2.all());
 
 ///////////////////////////////
 // Update
 ///////////////////////////////
-const update = db.prepare("UPDATE users SET age = 10 WHERE name = 'BBB';")
+const update: BetterSqlite3.Statement = db.prepare("UPDATE users SET age = 10 WHERE name = 'BBB';")
 update.run();
-console.log(stmt2.all());
+console.log(get2.all());
 
 ///////////////////////////////
 // Delete Record
 ///////////////////////////////
-const del = db.prepare("DELETE FROM users WHERE name = 'BBB';");
+const del: BetterSqlite3.Statement = db.prepare("DELETE FROM users WHERE name = 'BBB';");
 del.run();
-console.log(stmt2.all()); // return []
+console.log(get2.all()); // return []
 
 ///////////////////////////////
 // Delete Table
 ///////////////////////////////
 // 1. Delete Table Logical
-const delTable = db.prepare("DROP TABLE users;");
+const delTable: BetterSqlite3.Statement = db.prepare("DROP TABLE users;");
 delTable.run();
 // 2. Release Disk Space
-const vacuum = db.prepare("VACUUM;");
+const vacuum: BetterSqlite3.Statement = db.prepare("VACUUM;");
 vacuum.run();
